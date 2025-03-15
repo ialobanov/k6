edebb37e7eeffecba537309706505f7cf5853561
ialobanov <ivan.a.lobanov@gmail.com>
2025-03-15 21:29:18 +0300

    Work with repository

---
+++ test.js
@@ -0,0 +1,22 @@
+import http from 'k6/http';
+import { Rate } from 'k6/metrics';
+import { check } from 'k6';
+
+const failures = new Rate('falied_requests');
+
+export const options = {
+  vus: 10,
+  duration: '10s',
+  thresholds: {
+    http_req_failed: ['rate<=0'],
+    http_req_duration: ['p(95)<500']
+  }
+}
+
+export default function() {
+  const result = http.get('http://10.72.55.133:8000');
+  check(result, {
+    'http response status code is 200': r => r.status === 200,
+  });
+  failures.add(result.status !== 200);
+}
